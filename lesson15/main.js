99a07ca56443b6ac7d9a76c67c127e224d76c020
fatalnyishod <fatalnyishod@gmail.com>
2018-05-05 17:43:26 +0700

    lesson 15
    radio buttons

---
+++ lesson15/main.js
@@ -0,0 +1,19 @@
+var radio = document.getElementsByName('fruit');
+
+for (var i=0; i<radio.length; i++) {
+	radio[i].onchange = testRadio;
+}
+
+function testRadio() {
+	console.log (this.value);
+}
+
+document.getElementById('one').onclick = checkRadio;
+function checkRadio() {
+	for (var i=0; i<radio.length; i++) {
+		if (radio[i].checked) {
+			alert(radio[i].value);
+			break;
+		}
+	}
+}
